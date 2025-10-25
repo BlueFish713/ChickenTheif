87ff3797189b17642943aafd99bfffc7e890f344
BlueFish713 <kudong0909@gmail.com>
2025-10-25 16:46:35 +0900

    Map 및 빛 효과

---
+++ Assets/Scripts/Effect/GlobalLightControl.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(UnityEngine.Rendering.Universal.Light2D))]
+public class GlobalLightControl : MonoBehaviour
+{
+    public float duration = 5f;
+
+    [SerializeField] private Gradient gradient;
+    private UnityEngine.Rendering.Universal.Light2D _light;
+    private float _startTime;
+
+    private void Awake()
+    {
+        _light = GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+        _startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the time elapsed since the start time
+        var timeElapsed = Time.time - _startTime;
+        // Calculate the percentage based on the sine of the time elapsed
+        var percentage = Mathf.Sin(timeElapsed / duration * Mathf.PI * 2) * 0.5f + 0.5f;
+        // Clamp the percentage to be between 0 and 1
+        percentage = Mathf.Clamp01(percentage);
+
+        _light.color = gradient.Evaluate(percentage);
+    }
+}
+
