d2c103d55149389b1246b9fd5432ab53e9ed581f
BlueFish713 <kudong0909@gmail.com>
2025-10-25 13:54:21 +0900

    SerializedDictionary

--- Assets/Scripts/Repository.cs
+++ Assets/Scripts/Repository.cs
@@ -8,8 +8,8 @@ public class Repository : MonoBehaviour // 싱글톤
 {
     //public static Repository Instance { get; private set; }
 
-    [SerializedDictionary]
-    public Dictionary<FishType, int> price = new Dictionary<FishType, int>
+    [SerializedDictionary("FishType", "Price")]
+    public SerializedDictionary<FishType, int> price = new SerializedDictionary<FishType, int>
     {
         {FishType.StarFish, 1000},
         {FishType.BlueFish, 2000}
