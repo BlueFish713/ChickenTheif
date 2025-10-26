f956650e27394244f764988d6ff33a23e77f33e5
CTPR <kkaaiivv0208@gmail.com>
2025-10-26 14:41:04 +0900

    해와달

--- Assets/Scripts/SpeechBubble.cs
+++ Assets/Scripts/SpeechBubble.cs
@@ -7,7 +7,7 @@ public class SpeechBubble : MonoBehaviour
     public TMP_Text textMesh;
     public SpriteRenderer bubbleRenderer;
     public float showTime = 2f; // 말풍선 유지 시간
-    public Vector3 offset = new Vector3(0, 2f, 0); // 캐릭터 머리 위로 띄우기
+    public Vector3 offset = new Vector3(0, 2.5f, 0); // 캐릭터 머리 위로 띄우기
     private Transform target; // 따라다닐 대상
     public List<Sprite> bubble;
     List<float> metric = new List<float> {1.3f,3f,5f,7f,9f,11f,12.5f,99f};
---
+++ Assets/Scripts/SunAndMoon.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class SunAndMoon : MonoBehaviour
+{
+    public Sprite sun;
+    public Sprite moon;
+
+}
